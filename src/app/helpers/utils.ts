254f9503c1dd84364b185bc1aa4f8d8833944ec4
Michael <m.rachinskii@dunice.net>
2019-10-30 12:34:18 +0300

    fixed types

--- src/app/helpers/utils.ts
+++ src/app/helpers/utils.ts
@@ -4,13 +4,13 @@ export const csvJSON = (csv: string): string => {
   const lines = csv.split('\n');
   const headers = lines[0].split(',');
 
-  return JSON.stringify(lines.reduce((result, line: string, lineIndex: number): IParsedDataItem[] => {
+  return JSON.stringify(lines.reduce((result: IParsedDataItem[], line: string, lineIndex: number): IParsedDataItem[] => {
     if (line && lineIndex !== 0) {
       const currentLine = line.split(',');
       const obj = currentLine.reduce((accum: IParsedDataItem, stringItem: string, index: number) => {
         accum[headers[index]] = stringItem;
         return accum;
-      }, {});
+      }, {} as IParsedDataItem);
       result.push(obj);
     }
     return result;
